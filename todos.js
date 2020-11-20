0c34d55a4baf874424640a94c48223ad4f2ee03b
John Rybo <68517441+johnrybo@users.noreply.github.com>
2020-11-20 11:56:49 +0100

    todo list

---
+++ todos.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', main);
+
+function main() {
+
+    addEventListeners();
+}
+
+function addEventListeners(){
+
+    let addTodo = document.getElementById('addTodo');
+    addTodo.addEventListener('click', newListItem);
+
+}
+
+function newListItem(event) {
+
+    event.preventDefault();
+    
+    let dateInput = document.getElementById('dateInput').value;
+    let dateTextNode = document.createTextNode(dateInput);
+
+    let textInput = document.getElementById("textInput").value;
+    let textNode = document.createTextNode(textInput);
+
+    let todoList = document.getElementById('todoList');
+    let todoListItem = document.createElement("li");
+    todoListItem.classList.add('todoListItemStyle');
+    
+    todoList.appendChild(todoListItem);
+    todoListItem.appendChild(dateTextNode);
+    todoListItem.appendChild(textNode);
+  }
